647156ba382f002e25a7a4e9ac8427f10a75b53d
Steve Millidge <smillidge@users.noreply.github.com>
2018-03-30 22:14:46 +0100

    Merge pull request #1 from Cousjava/master
    PAYARA-2349 added ListStatistic type

--- src/main/java/org/glassfish/external/statistics/impl/StatisticImpl.java
+++ src/main/java/org/glassfish/external/statistics/impl/StatisticImpl.java
@@ -8,6 +8,7 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
+// Portions Copyright [2022] Payara Foundation and/or affiliates
 
 package org.glassfish.external.statistics.impl;
 import org.glassfish.external.statistics.Statistic;
@@ -36,7 +37,7 @@ public abstract class StatisticImpl implements Statistic {
 
     protected final Map<String, Object> statMap = new ConcurrentHashMap<String, Object> ();
     
-    protected static final String NEWLINE = System.getProperty( "line.separator" );
+    protected static final String NEWLINE = System.lineSeparator();
 
     protected StatisticImpl(String name, String unit, String desc, 
                           long start_time, long sample_time) {
@@ -106,6 +107,7 @@ public abstract class StatisticImpl implements Statistic {
         startTime = System.currentTimeMillis();
     }
 
+    @Override
     public synchronized String toString() {
         return "Statistic " + getClass().getName() + NEWLINE +
             "Name: " + getName() + NEWLINE +
